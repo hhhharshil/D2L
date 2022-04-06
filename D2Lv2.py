6c59c3d2ca7389505e8865debdf0cf884e7e6ccc
hhhharshil <73005050+hhhharshil@users.noreply.github.com>
2022-04-06 14:28:14 -0400

    Add files via upload
    v2 allows a file to be passed as an argument instead of manually entering IPs each time

---
+++ D2Lv2.py
@@ -0,0 +1,31 @@
+import argparse, pathlib
+import requests
+
+parser = argparse.ArgumentParser()
+parser.add_argument('file', type=pathlib.Path)
+args = parser.parse_args()
+
+with args.file.open('r') as file:
+    book = [line.rstrip('\n') for line in file]
+
+for address in book:
+    domain_to_ip = "http://ip-api.com/json/" + address
+        
+    d_r = requests.get(domain_to_ip)
+    d_response = d_r.json()
+    query = d_response["query"]
+    convert_ip = query
+
+    URL = "https://api.iplocation.net/?cmd=ip-country&ip=" + query
+    r = requests.get(URL)
+    response = r.json()
+
+    org =response["isp"]
+    country = response["country_name"]
+    ip = response["ip"]
+
+
+    print('IP: ' + str(ip))
+    print('Organization: ' + str(org))
+    print('Country: ' + str(country))
+    print('-----------------------------------------')
